9f7a035aaa27ed12f80faf8e47d497e2bfd1f36b
Michael <huff600@hotmail.com>
2016-06-17 10:28:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/huff6/localdatabasecalllog/DatabaseHelper.java
@@ -0,0 +1,73 @@
+package com.example.huff6.localdatabasecalllog;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ *
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "callLog.db";
+    public static final String TABLE_NAME = "callLog_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+    public static final String COL_3 = "PHONE";
+    public static final String COL_4 = "START_TIME";
+    public static final String COL_5 = "END_TIME";
+    public static final String COL_6 = "DURATION";
+    public static final String COL_7 = "NOTES";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_NAME + " (ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "NAME TEXT,PHONE TEXT,START_TIME TEXT,END_TIME TEXT,DURATION TEXT,NOTES TEXT)");
+        //db.execSQL("CREATE TABLE log_table (id INTEGER PRIMARY KEY, name TEXT, phone TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name,    String number,   String startTime,
+                              String endTime, String duration, String notes) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        contentValues.put(COL_3, number);
+        contentValues.put(COL_4, startTime);
+        contentValues.put(COL_5, endTime);
+        contentValues.put(COL_6, duration);
+        contentValues.put(COL_7, notes);
+
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("select * from " + TABLE_NAME, null);
+    }
+
+    /*
+    public Integer deleteData(String id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete(TABLE_NAME, "ID = ?", new String[] { id });
+    }
+    */
+
+    public void deleteAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        //db.execSQL("delete from " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/com/example/huff6/localdatabasecalllog/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.huff6.localdatabasecalllog;
+
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper myDb;
+    EditText textName, textPhone, textStartTime, textEndTime, textDuration, textNotes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        textName      = (EditText) findViewById(R.id.editText_name);
+        textPhone     = (EditText) findViewById(R.id.editText_phone);
+        textStartTime = (EditText) findViewById(R.id.editText_startTime);
+        textEndTime   = (EditText) findViewById(R.id.editText_endTime);
+        textDuration  = (EditText) findViewById(R.id.editText_duration);
+        textNotes     = (EditText) findViewById(R.id.editText_notes);
+    }
+
+    public void onClickAddData(View v) {
+        //boolean isInserted = myDb.insertData("Dummy", "123");
+        boolean isInserted = myDb.insertData(
+                textName.getText().toString(),
+                textPhone.getText().toString(),
+                textStartTime.getText().toString(),
+                textEndTime.getText().toString(),
+                textDuration.getText().toString(),
+                textNotes.getText().toString()
+        );
+
+
+        if (isInserted) {
+            Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Data NOT Inserted", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void onClickViewData(View v) {
+        Cursor resultCursor = myDb.getAllData();
+        System.out.println("Before get count");
+        if (resultCursor.getCount() == 0) {
+            System.out.println("empty list");
+            showMessage("Error", "Nothing found");
+            return;
+        }
+
+        // ELSE //////
+        System.out.println("before string builder");
+        System.out.println("COUNT IS " + resultCursor.getCount());
+        StringBuilder buffer = new StringBuilder();
+        while (resultCursor.moveToNext()) {
+            System.out.println("inside loop");
+            buffer.append("Id       : " ).append(resultCursor.getString(0)).append("\n");
+            buffer.append("Name     : " ).append(resultCursor.getString(1)).append("\n");
+            buffer.append("Number   : " ).append(resultCursor.getString(2)).append("\n");
+            buffer.append("S Time   : " ).append(resultCursor.getString(3)).append("\n");
+            buffer.append("E Time   : " ).append(resultCursor.getString(4)).append("\n");
+            buffer.append("Duration : " ).append(resultCursor.getString(5)).append("\n");
+            buffer.append("Notes    : " ).append(resultCursor.getString(6)).append("\n\n");
+        }
+
+        System.out.println("out of loop");
+
+        // show all data
+        showMessage("Data", buffer.toString());
+    }
+    public void onClickUpdateQueue(View v) {
+        // DO NOTHING
+    }
+
+    public void onClickSyncDatabase(View v) {
+        myDb.deleteAllData();
+    }
+
+
+
+
+    public void showMessage(String title, String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+
+        builder.show();
+    }
+}
